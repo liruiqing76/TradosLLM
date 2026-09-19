2035bb913f926448d3e2f670def0ff2d0501309b
liruiqing <603888266@qq.com>
2026-09-20 01:02:30 +0800

    WorkbenchAction: Home 工作台按钮挂自定义图标（Icon=Workbench_Action_Icon，同翻译中心风格 32px ICO 进 plugin.resources）；图标源图入库 docs/assets/icons

--- TradosToolkit/Action/WorkbenchAction.cs
+++ TradosToolkit/Action/WorkbenchAction.cs
@@ -8,7 +8,7 @@ namespace TradosToolkit.Action
     /// <summary>
     /// 首页 Home 选项卡"工作台"按钮：打开/激活工作台窗口。
     /// </summary>
-    [Action("TradosToolkit_Workbench", Name = "Workbench_Action_Name", Description = "Workbench_Action_Description")]
+    [Action("TradosToolkit_Workbench", Name = "Workbench_Action_Name", Description = "Workbench_Action_Description", Icon = "Workbench_Action_Icon")]
     [ActionLayout(typeof(ToolkitWorkbenchRibbonGroup), 10, DisplayType.Large)]
     public class WorkbenchAction : AbstractAction
     {
